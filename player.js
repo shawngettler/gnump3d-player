a508eadbf0dda6ead38bd7ab55bace6a6d8a4743
Shawn Gettler <shawn.gettler@gmail.com>
2018-10-23 16:43:09 -0400

    added basic player functions

--- player.js
+++ player.js
@@ -11,11 +11,43 @@ var playlist = [];
 
 
 
+// player internals
+var getPlayer = function() {
+    var player = document.getElementById("player");
+    player.volume = 0.3;
+
+    player.addEventListener("ended", function() {
+        playNextFile();
+    });
+};
+
+
+
 // player controls
 var playFiles = function(urls) {
     playlist = urls;
+    playNextFile();
 };
 
 var enqueueFiles = function(urls) {
     playlist = playlist.concat(urls);
 };
+
+var playNextFile = function() {
+    var player = document.getElementById("player");
+    if(playlist.length > 0) {
+        player.pause();
+        player.src = playlist.shift();
+        player.load();
+        player.play();
+    }
+};
+
+
+
+// run
+(function(){
+    window.addEventListener("load", function() {
+        getPlayer();
+    });
+})();
